4a1cf5634f54c82a38b6b966df4364525f2a5741
q526261747 <526261747@qq.com>
2017-09-28 10:32:58 +0800

    part2

--- app/src/main/java/cn/edu/gdmec/android/mobileguard/m1home/utils/MyUtils.java
+++ app/src/main/java/cn/edu/gdmec/android/mobileguard/m1home/utils/MyUtils.java
@@ -11,13 +11,12 @@ import android.content.pm.PackageManager;
 public class MyUtils {
     public static String getVersion(Context context){
         PackageManager packageManager = context.getPackageManager();
-        PackageInfo packageInfo = null;
         try {
-            packageInfo = packageManager.getPackageInfo(context.getPackageName(),0);
+            PackageInfo packageInfo = packageManager.getPackageInfo(context.getPackageName(),0);
+            return packageInfo.versionName;
         } catch (PackageManager.NameNotFoundException e) {
             e.printStackTrace();
             return "";
         }
-        return packageInfo.versionName;
     }
 }
--- app/src/main/java/cn/edu/gdmec/android/mobileguard/m1home/utils/VersionUpdateUtils.java
+++ app/src/main/java/cn/edu/gdmec/android/mobileguard/m1home/utils/VersionUpdateUtils.java
@@ -1,6 +1,12 @@
 package cn.edu.gdmec.android.mobileguard.m1home.utils;
 
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.os.Handler;
+import android.os.Message;
+import android.widget.Toast;
 
 import junit.runner.Version;
 
@@ -15,7 +21,8 @@ import org.json.JSONException;
 import org.json.JSONObject;
 
 import java.io.IOException;
-
+import cn.edu.gdmec.android.mobileguard.R;
+import cn.edu.gdmec.android.mobileguard.m1home.HomeActivity;
 import cn.edu.gdmec.android.mobileguard.m1home.entity.VersionEntity;
 
 /**
@@ -26,6 +33,35 @@ public class VersionUpdateUtils {
     private String mVersion;
     private Activity context;
     private VersionEntity versionEntity;
+
+    private static final int MESSAGE_IO_ERROR = 102;
+    private static final int MESSAGE_JSON_ERROR = 103;
+    private static final int MESSAGE_SHOW_DIALOG = 104;
+    private static final int MESSAGE_ENTERHOMR = 105;
+    
+    private Handler handler = new Handler(){
+        @Override
+        public void handleMessage(Message msg){
+            switch (msg.what){
+                case MESSAGE_IO_ERROR:
+                    Toast.makeText(context,"IO错误",Toast.LENGTH_LONG).show();
+                    break;
+                case MESSAGE_JSON_ERROR:
+                    Toast.makeText(context,"IO错误",Toast.LENGTH_LONG).show();
+                    break;
+                case MESSAGE_SHOW_DIALOG:
+                    showUPdateDialog(versionEntity);
+                    break;
+                case MESSAGE_ENTERHOMR:
+                    Intent intent = new Intent(context, HomeActivity.class);
+                    context.startActivity(intent);
+                    context.finish();
+                    break;
+            }
+        }
+    };
+
+
 
     public VersionUpdateUtils(String mVersion, Activity context){
         this.mVersion = mVersion;
@@ -48,16 +84,45 @@ public class VersionUpdateUtils {
                 versionEntity.apkurl = jsonObject.getString("apkurl");
                 if (!mVersion.equals(versionEntity.versioncode)){
                     //版本不同，需升级
-                    System.out.println(versionEntity.desciption);
-                    DownloadUtils downloadUtils = new DownloadUtils();
-                    downloadUtils.downloadAPK(versionEntity.apkurl,"mobileguard.apk",context);
+                    handler.sendEmptyMessage(MESSAGE_SHOW_DIALOG);
                 }
             }
         } catch (IOException e) {
+            handler.sendEmptyMessage(MESSAGE_IO_ERROR);
             e.printStackTrace();
         } catch (JSONException e) {
+            handler.sendEmptyMessage(MESSAGE_JSON_ERROR);
             e.printStackTrace();
         }
+
+    }
+    private void showUPdateDialog(final VersionEntity versionEntity) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(context);
+        builder.setTitle("检查到有新版本："+versionEntity.versioncode);
+        builder.setMessage(versionEntity.desciption);
+        builder.setCancelable(false);
+        builder.setIcon(R.mipmap.ic_launcher_round);
+        builder.setPositiveButton("立即升级", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                downloadNewApk(versionEntity.apkurl);
+            }
+        });
+        builder.setNegativeButton("暂不升级", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                dialogInterface.dismiss();
+                enterHome();
+            }
+        });
+        builder.show();
+    }
+    private void enterHome(){
+        handler.sendEmptyMessage(MESSAGE_ENTERHOMR);
+    }
+    private void downloadNewApk(String apkurl){
+        DownloadUtils downloadUtils = new DownloadUtils();
+        downloadUtils.downloadAPK(apkurl,"mobileguard.apk",context);
     }
 
 }
