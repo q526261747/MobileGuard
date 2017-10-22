8ced4124ccfffb9f658864b69ec852e9237cf999
q526261747 <526261747@qq.com>
2017-10-22 23:44:16 +0800

    手机防盗sim卡绑定

---
+++ app/src/main/java/cn/edu/gdmec/android/mobileguard/m2theftguard/entity/ContactInfo.java
@@ -0,0 +1,11 @@
+package cn.edu.gdmec.android.mobileguard.m2theftguard.entity;
+
+/**
+ * Created by 52626 on 2017/10/22.
+ */
+
+public class ContactInfo {
+    public String id;
+    public String name;
+    public String phone;
+}
