a4e8f46198bfb82989c5a556222bff283a1a2ea5
q526261747 <526261747@qq.com>
2017-11-03 22:06:31 +0800

    通讯功能实现

---
+++ app/src/main/java/cn/edu/gdmec/android/mobileguard/m3communicationguard/receiver/InterceptCallReciever.java
@@ -0,0 +1,16 @@
+package cn.edu.gdmec.android.mobileguard.m3communicationguard.receiver;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+
+/**
+ * Created by 52626 on 2017/11/1.
+ */
+
+public class InterceptCallReciever extends BroadcastReceiver{
+    @Override
+    public void onReceive(Context context, Intent intent) {
+
+    }
+}
---
+++ app/src/main/java/cn/edu/gdmec/android/mobileguard/m3communicationguard/receiver/InterceptSmsReciever.java
@@ -0,0 +1,8 @@
+package cn.edu.gdmec.android.mobileguard.m3communicationguard.receiver;
+
+/**
+ * Created by 52626 on 2017/11/1.
+ */
+
+public class InterceptSmsReciever {
+}
